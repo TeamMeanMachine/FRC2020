0742f9d2e127f692dcb99ccabb72fcd4fe74b352
jcrowell <jon@truegemonline.com>
2020-01-07 14:36:32 -0800

    remove meanlib subdirectory to properly set up as submodule

--- meanlib/src/main/kotlin/org/team2471/frc/lib/math/Range.kt
+++
@@ -1,6 +0,0 @@
-package org.team2471.frc.lib.math
-
-typealias DoubleRange = ClosedFloatingPointRange<Double>
-
-fun DoubleRange.intersects(other: DoubleRange): Boolean =
-        start in other || endInclusive in other
