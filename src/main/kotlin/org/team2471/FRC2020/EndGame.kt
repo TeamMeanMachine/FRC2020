c37b64b9a86435dda6ca20ed02b0bebf52252820
a-crowell <ayla.crowell@gmail.com>
2020-01-17 18:40:00 -0800

    Added EndGame Subsystem and started stub.

---
+++ src/main/kotlin/org/team2471/FRC2020/EndGame.kt
@@ -0,0 +1,19 @@
+package org.team2471.FRC2020
+
+import org.team2471.frc.lib.actuators.MotorController
+import org.team2471.frc.lib.actuators.SparkMaxID
+import org.team2471.frc.lib.actuators.SparkMaxWrapper
+import org.team2471.frc.lib.actuators.TalonID
+import org.team2471.frc.lib.framework.Subsystem
+
+object EndGame : Subsystem("EndGame") {
+//    private val balanceMotor = MotorController(TalonID(Talons.ENDGAME_BALANCE){ //Waiting for Robot Map
+//
+//    }
+
+//    private val climbRiseMotor = MotorController(SparkMaxID(SparkMaxWrapper.RISE_CLIMB)) { //Waiting for Robot Map
+//
+//    }
+
+
+}
