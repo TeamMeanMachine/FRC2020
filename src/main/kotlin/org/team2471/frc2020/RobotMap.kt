9c55b484aaf0d718ad862c82a6d96597a7f8b8c5
a-crowell <ayla.crowell@gmail.com>
2020-01-27 20:08:31 -0800

    Started Robot Map (swerves and a few others).

--- src/main/kotlin/org/team2471/frc2020/RobotMap.kt
+++ src/main/kotlin/org/team2471/frc2020/RobotMap.kt
@@ -3,23 +3,24 @@
 package org.team2471.frc2020
 
 object Talons {
-
+    const val INTAKE = 6
+    const val CONTROL_PANEL = 9
+    const val CLIMBER = 14
 }
 
 object Sparks {
-    const val DRIVE_FRONTLEFT = 1 //
-    const val DRIVE_BACKLEFT = 12 //
-    const val DRIVE_BACKRIGHT = 13 //
-    const val DRIVE_FRONTRIGHT = 16 //
+    const val DRIVE_FRONTLEFT = 12 //
+    const val DRIVE_BACKLEFT = 3 //
+    const val DRIVE_BACKRIGHT = 16 //
+    const val DRIVE_FRONTRIGHT = 15 //
 
-    const val STEER_FRONTLEFT = 5 //
-    const val STEER_BACKLEFT = 10 //
-    const val STEER_BACKRIGHT = 11 //
-    const val STEER_FRONTRIGHT = 4 //
+    const val STEER_FRONTLEFT = 8 //
+    const val STEER_BACKLEFT = 7 //
+    const val STEER_BACKRIGHT = 4 //
+    const val STEER_FRONTRIGHT = 11 //
 
-    const val SHOOTER = 3 //
-    const val SHOOTER2 = 6 //
-    const val CONTROL_PANEL = 9 //
+//    const val SHOOTER = 3 //
+//    const val SHOOTER2 = 6 //
 }
 
 object AnalogSensor {
